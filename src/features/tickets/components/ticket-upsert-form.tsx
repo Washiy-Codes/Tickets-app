94935412098221bd15eb3e5988d6b7cd1e1d4abb
Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-06-11 23:44:35 +0300

    feat: implement ticket upsert form with state handling

--- src/features/tickets/components/ticket-upsert-form.tsx
+++ src/features/tickets/components/ticket-upsert-form.tsx
@@ -6,28 +6,40 @@ import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { Ticket } from "@/app/generated/prisma/client"
 import { upsertTicket } from "../actions/upsert-ticket"
+import FieldError from "@/components/form/field-error"
 
 type TicketUpsertFormProps = {
   ticket?: Ticket
 };
 
 const TicketUpsertForm = ({ticket}: TicketUpsertFormProps) => {   
-    const [state, action] = useActionState(upsertTicket.bind(null, ticket?.id),{
-        message : ""
-    });
+    const [actionState, action] = useActionState(upsertTicket.bind(null, ticket?.id), {
+        message: "",
+        fieldErrors: {},
+    })
+
   return (
-    <form action={action} className="flex flex-col gap-y-1.5">
-     <input type="hidden" name="id" value={ticket?.id} /> 
-     <Label htmlFor='title'>Title</Label>
-     <Input id="title" name="title" type="text" defaultValue={
-      (state.payload?.get("title") as string) ?? ticket?.title} />
-     <Label htmlFor='content'>Content</Label>
-     <Textarea id="content" name="content" 
-     defaultValue={(state.payload?.get("content") as string) ?? ticket?.content} />
+    <form action={action} className="flex flex-col gap-3 my-2">
+     <Label htmlFor="title">Title</Label>
+     <Input 
+     id="title" 
+     name="title" 
+     type="text" 
+     defaultValue={(actionState.payload?.get("title") as string) ?? ticket?.title} />
+    <FieldError actionState={actionState} name="title" />
+
+     <Label htmlFor="content">Content</Label>
+     <Textarea 
+     id="content" 
+     name="content" 
+     defaultValue={(actionState.payload?.get("content") as string) ?? ticket?.content} />
+    <FieldError actionState={actionState} name="content" />
+
     <SubmitButton label={ticket ? "Edit" : "Create"} />
-    {state.message}
+    
+    {actionState.message}
     </form>
   )
 }
 
-export {TicketUpsertForm}
+export {TicketUpsertForm} 
